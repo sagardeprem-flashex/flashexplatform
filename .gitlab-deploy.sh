46897fc007f152b6cccb9b205ba155a9d60c1452
Saif.Farooqui <saif.farooqui@stackroute.in>
2019-11-18 14:07:11 +0530

    Update .gitlab-deploy.sh

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -12,6 +12,13 @@ echo "Deploying project on server ${server} as ${user} from branch ${branch}"
 
 apt-get update && apt-get install -y openssh-client
 
+apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt update
+apt-cache policy docker-ce
+apt install docker-ce
+
 ## Rolling Update
 
 command="ls -ltr && \
