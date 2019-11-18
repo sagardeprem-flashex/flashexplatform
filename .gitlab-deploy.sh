9a71dc314b7bd4df30feca1c3cc76af32f6f1ab4
Saif.Farooqui <saif.farooqui@stackroute.in>
2019-11-18 15:07:51 +0530

    Update .gitlab-deploy.sh

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -24,8 +24,8 @@ apt-cache search docker-ce
 apt update
 apt install docker-ce
 
-# curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-# chmod +x /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 ## Rolling Update
 
