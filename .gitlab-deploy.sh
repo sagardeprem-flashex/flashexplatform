2f26bea7b64e85a37b800b0f3c8fb454d1af0d63
Saif.Farooqui <saif.farooqui@stackroute.in>
2019-11-18 15:36:19 +0530

    Update .gitlab-deploy.sh

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -12,20 +12,20 @@ echo "Deploying project on server ${server} as ${user} from branch ${branch}"
 
 apt-get update && apt-get install -y openssh-client
 
-apt-get install -y gnupg2
-apt-get install -y curl
-dpkg -S add-apt-repository
+# apt-get install -y gnupg2
+# apt-get install -y curl
+# dpkg -S add-apt-repository
 
-apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-apt-cache search docker-ce
+# apt install apt-transport-https ca-certificates curl software-properties-common
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+# apt-cache search docker-ce
 
-apt update
-apt install docker-ce
+# apt update
+# apt install docker-ce
 
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+# curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+# chmod +x /usr/local/bin/docker-compose
 
 ## Rolling Update
 
