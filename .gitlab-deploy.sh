0e7bf474d2a07eb0bf6e7b77032a947325125d59
Saif.Farooqui <saif.farooqui@stackroute.in>
2019-11-18 14:33:38 +0530

    Update .gitlab-deploy.sh

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -13,20 +13,11 @@ echo "Deploying project on server ${server} as ${user} from branch ${branch}"
 apt-get update && apt-get install -y openssh-client
 
 apt-get install -y gnupg2
-apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   trusty \
-   stable"
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io       
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic test"
+apt update
+apt install docker-ce
 
 ## Rolling Update
 
