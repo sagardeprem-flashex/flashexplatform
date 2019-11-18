32012bd063e0ad80cdaeffa27a51d1a35decc64f
Saif.Farooqui <saif.farooqui@stackroute.in>
2019-11-18 14:52:23 +0530

    Update .gitlab-deploy.sh

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -16,7 +16,9 @@ apt-get install -y gnupg2
 apt install curl
 apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic test"
+# add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu artful stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu artful stable"
+
 apt update
 apt install docker-ce
 
