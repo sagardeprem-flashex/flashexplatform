7dbb2ba571042ef220a4c98412eeb8a86e4cbbe5
Saif.Farooqui <saif.farooqui@stackroute.in>
2019-11-18 14:30:50 +0530

    Update .gitlab-deploy.sh

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -31,7 +31,7 @@ apt-get install docker-ce docker-ce-cli containerd.io
 ## Rolling Update
 
 command="ls -ltr /home && \
- cd /home/ssm-user && \
+ cd /home/ubuntu && \
  rm -rf flashexplatform && \
  git clone https://${gituser}:${gittoken}@gitlab.stackroute.in/flashex/flashexplatform.git -b ${branch} && \
  cd flashexplatform && \
