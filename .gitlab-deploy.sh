90b7fb205d4eaa32d46e8d446270e94beeae24c4
Saif.Farooqui <saif.farooqui@stackroute.in>
2019-11-18 14:23:59 +0530

    Update .gitlab-deploy.sh

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -23,7 +23,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   artful \
+   trusty \
    stable"
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io       
