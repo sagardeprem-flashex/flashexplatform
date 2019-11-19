a005adcf8b1920a76c437b343f06e6fbff497abe
Game <kumar.prabhat@boeing.com>
2019-11-19 12:37:27 +0530

    tracking service UI has been implemented with two set of trips with karma test being passed

--- common-ms/flashex-webapp/angular-boiler-plate/src/app/trip-management/components/live-tracking/live-tracking.component.ts
+++ common-ms/flashex-webapp/angular-boiler-plate/src/app/trip-management/components/live-tracking/live-tracking.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { TripItineraryService } from '../../services/trip-itinerary.service';
-import { MarkerLabel } from '../../interfaces/markerlabel';
 
 
 @Component({
--- common-ms/flashex-webapp/angular-boiler-plate/src/app/trip-management/services/trip-itinerary.service.spec.ts
+++ common-ms/flashex-webapp/angular-boiler-plate/src/app/trip-management/services/trip-itinerary.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TripItineraryService } from './trip-itinerary.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('TripItineraryService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule]
+  }));
 
   it('should be created', () => {
     const service: TripItineraryService = TestBed.get(TripItineraryService);
