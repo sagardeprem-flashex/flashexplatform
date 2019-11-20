35008e504dba4cce5a273fb7cb5aadaba65cd270
sudhanshu kulkarni <sudhanshu.kulkarni@boeing.com>
2019-11-20 14:16:31 +0530

    order management ms: replaced with shipment management, added a shipment worker service'

---
+++ shipment-ms/shipment-web-service/src/main/java/com/flashex/shipmentmicroservice/webservice/DemoApplication.java
@@ -0,0 +1,35 @@
+package com.flashex.shipmentmicroservice.webservice;
+
+
+
+import com.flashex.shipmentmicroservice.lib.services.SampleService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = "com.flashex.ordermicroservice")
+@RestController
+public class DemoApplication {
+
+    private final SampleService sampleService;
+
+    public DemoApplication(SampleService sampleService) {
+        this.sampleService = sampleService;
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return sampleService.message();
+    }
+    @GetMapping("/")
+    public String home1() {
+        return "BLANK";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
