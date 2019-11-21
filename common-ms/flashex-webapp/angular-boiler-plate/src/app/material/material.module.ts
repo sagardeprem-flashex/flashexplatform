9bb1227ae7d7e279d78bb7aaa4cd6dab72ab37cc
Game <kumar.prabhat@boeing.com>
2019-11-21 16:41:34 +0530

    stepper included to show delivery status

--- common-ms/flashex-webapp/angular-boiler-plate/src/app/material/material.module.ts
+++ common-ms/flashex-webapp/angular-boiler-plate/src/app/material/material.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
-import { MatSidenavModule, MatExpansionModule} from '@angular/material';
+import { MatSidenavModule, MatExpansionModule, MatStepperModule} from '@angular/material';
 
 const MaterialComponents = [
   MatToolbarModule,
   MatButtonModule,
   MatSidenavModule,
-  MatExpansionModule
+  MatExpansionModule,
+  MatStepperModule
 ];
 
 @NgModule({
-  imports: [ MatToolbarModule, MatButtonModule, MatSidenavModule, MatExpansionModule ],
-  exports: [   MatToolbarModule, MatButtonModule, MatSidenavModule, MatExpansionModule ]
+  imports: [ MatToolbarModule, MatButtonModule, MatSidenavModule, MatExpansionModule, MatStepperModule ],
+  exports: [ MatToolbarModule, MatButtonModule, MatSidenavModule, MatExpansionModule, MatStepperModule ]
 })
 export class MaterialModule { }
