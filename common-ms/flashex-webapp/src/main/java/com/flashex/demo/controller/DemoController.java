27085b0d144ffa684cc3da2b584b293dd44a143c
Saif Ahmad <saif.farooqui@stackroute.in>
2019-11-22 18:27:20 +0530

    removed hash routing from angular

--- common-ms/flashex-webapp/src/main/java/com/flashex/demo/controller/DemoController.java
+++ common-ms/flashex-webapp/src/main/java/com/flashex/demo/controller/DemoController.java
@@ -1,12 +1,20 @@
 package com.flashex.demo.controller;
 
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RefreshScope
-@RestController
+@Controller
 public class DemoController {
+
+    @RequestMapping(value = "/**/{[path:[^\\.]*}")
+    public String redirectToAngular() {
+        return "forward:/";
+    }
+
     @GetMapping("/hello")
     public  String hello() {
         System.out.println("In demo controller");
