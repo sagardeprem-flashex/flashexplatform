4ab996a17dce847e1eb014043a65b5e42665b2d9
Saif.Farooqui <saif.farooqui@stackroute.in>
2019-11-24 00:17:52 +0530

    Update common-ms/flashex-webapp/angular-boiler-plate/karma.conf.js

--- common-ms/flashex-webapp/angular-boiler-plate/karma.conf.js
+++ common-ms/flashex-webapp/angular-boiler-plate/karma.conf.js
@@ -25,7 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless', 'MyHeadlessChrome'],
+    customLaunchers: {
+      MyHeadlessChrome: {
+        base: 'ChromeHeadless',
+        flags: ['--disable-translate', '--disable-extensions', '--remote-debugging-port=9223']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
