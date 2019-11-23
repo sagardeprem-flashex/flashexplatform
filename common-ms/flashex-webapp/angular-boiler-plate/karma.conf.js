20881e089877f713234c118567998e2aea27d35d
Saif.Farooqui <saif.farooqui@stackroute.in>
2019-11-24 01:21:13 +0530

    Update common-ms/flashex-webapp/angular-boiler-plate/karma.conf.js

--- common-ms/flashex-webapp/angular-boiler-plate/karma.conf.js
+++ common-ms/flashex-webapp/angular-boiler-plate/karma.conf.js
@@ -39,6 +39,9 @@ module.exports = function (config) {
       }
     },
     browserNoActivityTimeout: 50000,
+    captureTimeout: 60000, // it was already there
+    browserDisconnectTimeout : 60000,
+    browserDisconnectTolerance : 1,
     singleRun: false,
     restartOnFileChange: true
   });
