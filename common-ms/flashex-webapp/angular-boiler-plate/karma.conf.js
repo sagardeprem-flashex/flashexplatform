1af68a2ed81018c1d20c83e19bb2ea1570320af7
Saif.Farooqui <saif.farooqui@stackroute.in>
2019-11-24 15:56:05 +0530

    Update common-ms/flashex-webapp/angular-boiler-plate/karma.conf.js, common-ms/flashex-webapp/angular-boiler-plate/package.json files

--- common-ms/flashex-webapp/angular-boiler-plate/karma.conf.js
+++ common-ms/flashex-webapp/angular-boiler-plate/karma.conf.js
@@ -1,8 +1,5 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-const puppeteer = require('puppeteer');
-process.env.CHROME_BIN = puppeteer.executablePath();
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -34,7 +31,6 @@ module.exports = function (config) {
         flags: [
           '--headless',
           '--disable-gpu',
-          '--js-flags=--max_old_space_size=4096',
           '--remote-debugging-port=9223',
           '--no-sandbox'
         ],
