8a3090006b49a55fe62744a19c2065148acc2454
sudhanshu kulkarni <sudhanshu.kulkarni@boeing.com>
2019-11-28 16:10:18 +0530

    Karma tests in Orderdetailscomponent and ShipmentManagementService cleared

--- common-ms/flashex-webapp/angular-boiler-plate/src/app/trip-management/services/shipment-management.service.spec.ts
+++ common-ms/flashex-webapp/angular-boiler-plate/src/app/trip-management/services/shipment-management.service.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ShipmentManagementService } from './shipment-management.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 describe('ShipmentManagementService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientModule]
+  }));
   it('should be created', () => {
     const service: ShipmentManagementService = TestBed.get(ShipmentManagementService);
     expect(service).toBeTruthy();
