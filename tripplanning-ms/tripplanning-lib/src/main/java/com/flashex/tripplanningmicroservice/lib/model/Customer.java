875accfd1c5d66e3258503600161e268819d0cf2
Aejit <aejit.tripathi@boeing.com>
2019-12-03 18:13:15 +0530

    I added test cases for tripplanning lib repo and service layer using juin 5

--- tripplanning-ms/tripplanning-lib/src/main/java/com/flashex/tripplanningmicroservice/lib/model/Customer.java
+++ tripplanning-ms/tripplanning-lib/src/main/java/com/flashex/tripplanningmicroservice/lib/model/Customer.java
@@ -1,19 +1,35 @@
 package com.flashex.tripplanningmicroservice.lib.model;
 
+import com.datastax.driver.core.DataType;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.CassandraType;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.Table;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("customer")
 public class Customer {
 
     /** String variables **/
+    @PrimaryKeyColumn(name = "customerfirstname",  ordinal = 0, type = PrimaryKeyType.PARTITIONED)
+    @CassandraType(type = DataType.Name.TEXT)
     public String firstName;
+    @CassandraType(type = DataType.Name.TEXT)
     public String middleName;
+    @CassandraType(type = DataType.Name.TEXT)
     public String lastName;
+    @CassandraType(type = DataType.Name.TEXT)
     public String emailId;
 
     /** Integer variables **/
+    @CassandraType(type = DataType.Name.INT)
     public int phoneNumber;
 
 }
--- tripplanning-ms/tripplanning-lib/src/main/java/com/flashex/tripplanningmicroservice/lib/services/TripItineraryService.java
+++ tripplanning-ms/tripplanning-lib/src/main/java/com/flashex/tripplanningmicroservice/lib/services/TripItineraryService.java
@@ -1,5 +1,6 @@
 package com.flashex.tripplanningmicroservice.lib.services;
 
+import com.datastax.driver.core.Session;
 import com.flashex.tripplanningmicroservice.lib.model.TripItinerary;
 import com.flashex.tripplanningmicroservice.lib.repository.TripItineraryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +14,17 @@ public class TripItineraryService {
     @Autowired
     private TripItineraryRepository tripItineraryRepository;
 
+
     public TripItinerary getSpecificTripItinerary(String id) {
-        if(this.tripItineraryRepository.findById(id).isPresent()) {
-            return this.tripItineraryRepository.findById(id).get();
-        }
-        return null;
+//        if(this.tripItineraryRepository.findById(id).isPresent()) {
+//            return this.tripItineraryRepository.findById(id).get();
+//        }
+//        else
+//        return null;
+        return this.tripItineraryRepository.findById(id).orElse(null);
     }
+
+
 
     public List<TripItinerary> getAllTripItineraries()
     {
@@ -34,5 +40,6 @@ public class TripItineraryService {
 
         this.tripItineraryRepository.delete(tripItinerary);
     }
+
 
 }
