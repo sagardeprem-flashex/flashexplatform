ebcbb1b700b58cbbdd398fdbf25fc853527d3a4d
Prem <premsagar.shankaraiah@boeing.com>
2019-12-04 10:13:26 +0530

    Shipment generation working fine, test cases pending

--- shipment-ms/shipment-lib/src/main/java/com/flashex/shipmentmicroservice/lib/services/BinningService.java
+++ shipment-ms/shipment-lib/src/main/java/com/flashex/shipmentmicroservice/lib/services/BinningService.java
@@ -5,6 +5,8 @@ import com.flashex.shipmentmicroservice.lib.model.BinnerConfig;
 import com.flashex.shipmentmicroservice.lib.model.Packet;
 import com.flashex.shipmentmicroservice.lib.model.Shipment;
 import org.apache.commons.collections4.ListUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -13,6 +15,9 @@ import java.util.*;
 public class BinningService {
 
 
+    private  static final Logger logger = (Logger) LoggerFactory.getLogger(BinningService.class);
+
+    // Local list of bins
     private static List<Bin> bins = new ArrayList<>();
 
     public static List<Bin> getBins() {
@@ -24,13 +29,14 @@ public class BinningService {
     }
 
 
+    // adds a packet to a bin
     public void binPacket(Packet packet){
 
-        // get binning properties
+        // get binning properties to decide which bin to add to
         List<String> packetProperties = getPacketProperties(packet);
         System.out.println(packetProperties.toString());
 
-        // find which bin to add
+        // find bin based on properties
         int binIndex = this.getBinIndex(packetProperties);
 
         // add to bin
@@ -38,35 +44,38 @@ public class BinningService {
 
     }
 
+    // generates shipments from all bins
     public List<Shipment> generateShipment(){
+
+        // list of all shipments
         List<Shipment> shipments = new ArrayList<>();
 
         if(bins.size()!=0){
 
             for(int i=0; i<bins.size();i++){
-                // generate shipments
-                List<List<Packet>> generatedPacketLists = ListUtils.partition(bins.get(i).getBinnedPackets(),getConfig().getMaxShipmentSize());
-                System.out.println("#########################Generating shipments################################");
-                System.out.println(generatedPacketLists.toString());
-                System.out.println("########################################################");
 
+                // create sublist for each bin for adding to shipments
+                List<List<Packet>> generatedPacketLists = ListUtils.partition(bins.get(i).getBinnedPackets(),getConfig().getMaxShipmentSize());
+
+                // for each sublist, create a shipment
                 for(int j = 0; j<generatedPacketLists.size(); j++){
                     Shipment shipment = new Shipment();
-
                     shipment.setPacketList(sortPacketList(generatedPacketLists.get(j), getConfig().getSortBy()));
                     shipment.setShipmentDate(new Date());
                     shipment.setShipmentId(UUID.randomUUID().toString());
                     shipments.add(shipment);
                 }
-
-
-                // clear bin on generating the shipment
-                bins.get(i).getBinnedPackets().clear();
             }
+
+            // clear bin on generating the shipment
+            bins.forEach(bin -> {
+                bin.setBinnedPackets(new ArrayList<>());
+            });
         }
         return shipments;
     }
 
+    // create a bin based on a certain grouping/binning strategy and sorting strategy
     public void createBin(List<String> binningStrategy, String sortingStrategy){
         Bin bin = new Bin();
         bin.setBinningStrategy(binningStrategy);
@@ -76,15 +85,15 @@ public class BinningService {
         bins.add(bin);
     }
 
-
+    // gets properties from a packet to decide which bin it will go to
     public List<String> getPacketProperties(Packet packet){
+
         List<String> groupBy = getConfig().getGroupStrategy();
         List<String> packetProperty = new ArrayList<>();
 
         for(String groupByField: groupBy){
             if(groupByField=="PINCODE"){
                 packetProperty.add(Long.toString(packet.getDeliveryAddress().getPincode()));
-//                packetProperty.add(Integer.toString(packet.getDeliveryAddress().getPincode()));
             } else if(groupByField=="PACKET_TYPE"){
                 packetProperty.add(packet.getPacketType());
             }
@@ -94,35 +103,41 @@ public class BinningService {
 
 
     public int getBinIndex(List<String> packetProperties){
-        System.out.println(">>>>>>>>>>>>>>>Getting bin index >>>>>>>>>>>");
-
+        logger.debug("Getting the bin index......");
         int binIndex = 0;
         if(bins.size()>0){
+            // if there are bins already, go ahead
             for (int i=0; i < bins.size();i++){
+                // check if a bin exists for the given properties
                 if(bins.get(i).getBinningStrategy().equals(packetProperties) ){
                     binIndex=i;
                 } else {
+                    // else create a bin
                     createBin(packetProperties, getConfig().getSortBy());
                     binIndex=bins.size()-1;
                 }
             }
         }else {
+            // if bins is empty, create a bin with given properties
             createBin(packetProperties, getConfig().getSortBy());
         }
-
-        System.out.println(">>>>>>>>>>>>>>>Getting bin index done >>>>>>>>>>>"+binIndex);
+        logger.debug("Bin index: {}", binIndex);
        return binIndex;
     }
 
+    // sorts a list of packets with the given strategy
     public List<Packet> sortPacketList(List<Packet> packetList,String sortBy){
 
         List<Packet> sortedPackets = packetList;
         if(sortBy=="RECEIVED_DATE"){
             sortedPackets.sort(Comparator.comparing(Packet::getReceivedDate));
         }
+        // more to come
         return sortedPackets;
     }
 
+
+    // gives the config to create bins, hard coded for now
     public BinnerConfig getConfig(){
 
         BinnerConfig config = new BinnerConfig();
