0f7f9de901073f481ad0486832eb586740411bb9
Prem <premsagar.shankaraiah@boeing.com>
2019-12-04 10:19:10 +0530

    Readded the flashex webapp files

---
+++ common-ms/flashex-webapp/src/main/java/com/flashex/demo/controller/DemoController.java
@@ -0,0 +1,23 @@
+package com.flashex.demo.controller;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RefreshScope
+@Controller
+public class DemoController {
+
+    @RequestMapping(value = "/**/{[path:[^\\.]*}")
+    public String redirectToAngular() {
+        return "forward:/";
+    }
+
+    @GetMapping("/hello")
+    public  String hello() {
+        System.out.println("In demo controller");
+        return "hi working";
+    }
+
+}
---
+++ common-ms/flashex-webapp/src/test/java/com/flashex/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashex.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
