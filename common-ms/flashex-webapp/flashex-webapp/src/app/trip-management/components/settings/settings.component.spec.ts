22e7abcf715170d68919cc41d213a609d8f5f9dd
Prem <premsagar.shankaraiah@boeing.com>
2019-12-12 10:08:52 +0530

    Fixed karma issues in settings component

--- common-ms/flashex-webapp/flashex-webapp/src/app/trip-management/components/settings/settings.component.spec.ts
+++ common-ms/flashex-webapp/flashex-webapp/src/app/trip-management/components/settings/settings.component.spec.ts
@@ -3,6 +3,10 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SettingsComponent } from './settings.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MaterialModule } from 'src/app/material/material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { OrderDetailsComponent } from '../order-details/order-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
@@ -13,7 +17,22 @@ describe('SettingsComponent', () => {
       declarations: [ SettingsComponent ],
       imports: [
         HttpClientTestingModule,
-        MaterialModule
+        MaterialModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        {
+          provide: MatDialogRef,
+          // useValues: {}
+          useValue: OrderDetailsComponent
+        },
+        // {
+        //   provide: MatDialogRef,
+        //   useValue: true
+        // }
+        { provide: MAT_DIALOG_DATA, useValue: {} },
       ]
     })
     .compileComponents();
