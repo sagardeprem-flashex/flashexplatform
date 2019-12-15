3cb5e57bb0231d310bb38499b9c68180a5fa554c
Game <kumar.prabhat@boeing.com>
2019-12-15 21:56:28 +0530

    added tomtom in delivery executive with markers

--- common-ms/flashex-webapp/flashex-webapp/src/app/delivery-executive/components/trips/trips.component.ts
+++ common-ms/flashex-webapp/flashex-webapp/src/app/delivery-executive/components/trips/trips.component.ts
@@ -39,6 +39,28 @@ export class TripsComponent implements OnInit {
   public scheduledDate = new Date();
   public intialData;
   public tripDate = new Date().toDateString();
+  public warehouse;
+  public centerMap;
+  public marks = [];
+  public location;
+  public markers = [];
+  public colors = [];
+  public color;
+  public markerIcon = ['../../../../assets/mapIcon/78753.svg',
+    '../../../../assets/mapIcon/2.svg',
+    '../../../../assets/mapIcon/3.svg',
+    '../../../../assets/mapIcon/4.svg',
+    '../../../../assets/mapIcon/5.svg',
+    '../../../../assets/mapIcon/6.svg',
+    '../../../../assets/mapIcon/7.svg',
+    '../../../../assets/mapIcon/8.svg',
+    '../../../../assets/mapIcon/9.svg',
+    '../../../../assets/mapIcon/10.svg',
+  ];
+  public addressLine = [];
+  public routeColor = ['blue', 'red', 'green', 'black'];
+
+
 
   constructor(changeDetectorRef: ChangeDetectorRef,
               media: MediaMatcher, private tripService: TriplogService,
@@ -59,15 +81,6 @@ export class TripsComponent implements OnInit {
       this.role = 'Delivery Executive';
 
     }
-
-    setTimeout(() => {
-      const map = tomtom.L.map('map', {
-        key: 'bvlnbSj7Eu5i41bgOFAlfWPZEuPkDcug',
-        basePath: '/assets/sdk',
-        center: [52.360306, 4.876935],
-        zoom: 15
-      });
-    }, 500);
 
     // tslint:disable-next-line: deprecation
     this.mobileQuery.removeListener(this.mobileQueryListener);
@@ -169,6 +182,87 @@ export class TripsComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
     });
+  }
+  loadMap() {
+      setTimeout(() => {
+      const map = tomtom.L.map('map', {
+        key: 'bvlnbSj7Eu5i41bgOFAlfWPZEuPkDcug',
+        basePath: '/assets/sdk',
+        center: this.centerMap,
+        zoom: 18,
+      });
+      // tslint:disable-next-line: prefer-for-of
+      for (let i = 0; i < this.dataSource.length; i++) {
+        this.marks = [];
+        this.warehouse = [
+          this.dataSource[i].originAddress.latitude,
+          this.dataSource[i].originAddress.longitude
+        ];
+        const packets = this.dataSource[i].packetLogs;
+        // store delivery address and latitude and longitude to marks
+        // tslint:disable-next-line: prefer-for-of
+        for (let j = 0; j < packets.length; j++) {
+          const deliveryAddress = packets[j].deliveryAddress;
+          const mark = [deliveryAddress.latitude, deliveryAddress.longitude];
+          this.centerMap = mark;
+          const address = deliveryAddress.addressLine1;
+          this.marks.push(mark);
+          this.addressLine.push(address);
+        }
+        const warehouseMarker: any = tomtom.L.marker(this.warehouse, {
+          icon: tomtom.L.icon({
+            iconUrl: '../../../../assets/images/warehouse.png',
+            iconSize: [40, 40],
+            iconAnchor: [30, 30],
+            popupAnchor: [0, -30]
+          }),
+        }).addTo(map);
+        warehouseMarker.bindPopup(this.addressLine[i]).openPopup();
+        // add marker to the map and attached delivery address to each marker
+        // tslint:disable-next-line: prefer-for-of
+        for (let m = 0; m < this.marks.length; m++) {
+          const marker: any = tomtom.L.marker(this.marks[m], {
+            icon: tomtom.L.icon({
+              iconUrl: this.markerIcon[i],
+              iconSize: [40, 40],
+              iconAnchor: [30, 30],
+              popupAnchor: [0, -30]
+            }),
+          }).addTo(map);
+          marker.bindPopup(this.addressLine[i]).openPopup();
+
+        }
+        const routesColor = this.routeColor[i];
+        const wareRoutes = this.warehouse.join(',').concat(':').concat(this.marks[0].join(','));
+        tomtom.routing().locations(wareRoutes)
+          // tslint:disable-next-line: only-arrow-functions
+          .go().then(function(routeJson) {
+            const route = tomtom.L.geoJson(routeJson, {
+              style: { color: routesColor, opacity: 0.5, weight: 5 }
+            }).addTo(map);
+            map.fitBounds(route.getBounds(), { padding: [5, 5] });
+          });
+        // tslint:disable-next-line: prefer-for-of
+        for (let n = 0; n < this.marks.length - 1; n++) {
+          // store origin and destination for routes
+          let routes = [];
+          routes = this.marks[n].join(',').concat(':').concat(this.marks[n + 1].join(','));
+          tomtom.routing().locations(routes)
+            // tslint:disable-next-line: only-arrow-functions
+            .go().then(function(routeJson) {
+              const route = tomtom.L.geoJson(routeJson, {
+                style: { color: routesColor, opacity: 0.5, weight: 5 }
+              }).addTo(map);
+              map.fitBounds(route.getBounds(), { padding: [5, 5] });
+            });
+
+        }
+      }
+    }, 1000);
+
+
+
+
   }
 
 }
